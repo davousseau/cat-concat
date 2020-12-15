d3f538e9820a1ed116b96a98d8d7e32b4bab4c8f
dbrsseau <dbrsseau@gmail.com>
2020-12-15 05:32:01 -0500

    Finish the concat command

--- src/concat.c
+++ src/concat.c
@@ -12,11 +12,60 @@
 #include <string.h>
 
 /**
+ * Get the file size.
+ * @param file File to seek
+ */
+long getFileSize(FILE *file)
+{
+    fseek(file, 0L, SEEK_END);
+    long int size = ftell(file) + 1;
+    rewind(file);
+    return size;
+}
+
+/**
+ * Write the contents of a file to an output file.
+ * @param inputFilename Input filename
+ * @param outputFile Output file
+ */
+void writeFile(const char *inputFilename, FILE *outputFile)
+{
+    FILE *file = fopen(inputFilename, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "concat: %s: %s\n", inputFilename, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    long size = getFileSize(file);
+    char buffer[size];
+    while (fgets(buffer, size, (FILE *)file))
+    {
+        fputs(buffer, outputFile);
+    }
+    fclose(file);
+}
+
+/**
  * Launch the concat command.
  * @param argc Number of parameter
  * @param argv Files to merge
  * @return Exit status
  */
-int main(int argc, const char *argv[]) {
-    // TODO:
+int main(int argc, const char *argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: ./concat [<input-filename-1>] [<input-filename-2>] [<output-filename>]\n");
+        exit(EXIT_FAILURE);
+    }
+    FILE *file = fopen(argv[3], "w");
+    if (file == NULL)
+    {
+        fprintf(stderr, "concat: %s: %s\n", argv[3], strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    writeFile(argv[1], file);
+    writeFile(argv[2], file);
+    fclose(file);
+    return EXIT_SUCCESS;
 }
