c3be8055e730106f3e6de0c4354223a085eb9703
dbrsseau <dbrsseau@gmail.com>
2020-12-24 03:28:41 -0500

    Fix typo

--- src/concat.c
+++ src/concat.c
@@ -55,7 +55,7 @@ int main(int argc, const char *argv[])
 {
     if (argc != 4)
     {
-        fprintf(stderr, "Use: bin/concat <input-filename> <input-filename> <output-filename>\n");
+        fprintf(stderr, "Usage: bin/concat <input-filename> <input-filename> <output-filename>\n");
         exit(EXIT_FAILURE);
     }
     FILE *file = fopen(argv[3], "w");
